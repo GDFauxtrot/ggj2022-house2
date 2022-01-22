f5fdb516da842fc20611dc8e4c7da54ed99ad84e
Weiyan Zhu <weiyanz@berkeley.edu>
2022-01-21 22:22:13 -0800

    shop select item

--- Assets/Wayne/Shop/ShopItemSelection.cs
+++ Assets/Wayne/Shop/ShopItemSelection.cs
@@ -5,11 +5,17 @@ using UnityEngine;
 public class ShopItemSelection : MonoBehaviour
 {
     private ShopItem itemData;
+    private ShopManager manager;
     [SerializeField] private Transform itemIconTransform;
-    public void Initialize(ShopItem itemData)
+    public void Initialize(ShopItem itemData, ShopManager manager)
     {
+        this.manager = manager;
         this.itemData = itemData;
         GameObject obj = Instantiate(itemData.iconPref, itemIconTransform);
         obj.transform.localPosition = Vector3.zero;
+    }
+
+    public void Select(){
+        manager.DisplayItemInfo(itemData);
     }
 }
--- Assets/Wayne/Shop/ShopManager.cs
+++ Assets/Wayne/Shop/ShopManager.cs
@@ -12,9 +12,30 @@ public class ShopManager : MonoBehaviour
     [SerializeField] private Transform itemSelectionGroup; 
     [SerializeField] private GameObject itemSelectionIconPref;
 
+    private ShopItem itemSelected;
+    private GameObject itemIcon;
+
     void Start()
     {
+        Open();
+    }
+
+    void Update()
+    {
+        if(itemIcon != null){
+            itemIcon.transform.Rotate(Vector3.up * Time.deltaTime * 60);
+        }
+    }
+
+    private void Open()
+    {
         InitializeSelectionPanel();
+        DisplayItemInfo(itemsAvailable[0]);
+    }
+
+    private void Close()
+    {
+
     }
 
     private void InitializeSelectionPanel(){
@@ -28,7 +49,18 @@ public class ShopManager : MonoBehaviour
         foreach(ShopItem i in itemsAvailable){
             GameObject newObj = Instantiate(itemSelectionIconPref, itemSelectionGroup);
             ShopItemSelection itemSelection = newObj.GetComponent<ShopItemSelection>();
-            itemSelection.Initialize(i);
+            itemSelection.Initialize(i, this);
         }
+    }
+
+    //Update item info to show a new item
+    public void DisplayItemInfo(ShopItem itemData){
+        itemSelected = itemData;
+        priceText.text = itemData.price.ToString();
+        // remove previous item icon 
+        if(itemIcon != null)
+            Destroy(itemIcon);
+        itemIcon = Instantiate(itemData.iconPref, itemIconTransform);
+        itemIcon.transform.localPosition = Vector3.zero;
     }
 }
