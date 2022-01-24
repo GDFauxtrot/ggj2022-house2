28d9ab32bc7d48822108dd378f0a549b634d32bb
GDFauxtrot <gdfauxtrot@gmail.com>
2022-01-24 03:00:27 -0800

    Improved player controller, shooting, better camera system
      * Massively fleshed out PlayerController.cs to support shooting projectiles, clamped movement
        * Note - projectile pool is super half-baked and needs work, it's pretty late rn so I can't finish it
      * Created Projectile.cs for some projectile logic, super basic
      * Greatly improved CameraController.cs to include variable angle, screen shake, and more fun parameters to play around with
      * Added layers for Player and projectiles (player and enemy)

--- Assets/Scripts/Gameplay/PlayerController.cs
+++ Assets/Scripts/Gameplay/PlayerController.cs
@@ -4,8 +4,38 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    [Header("Movement")]
     public Rigidbody rigidbody;
     public float speed;
+
+    [Header("Shooting")]
+    public float shootFireRate;
+
+    [Header("Projectiles")]
+    public float projectileSpeed;
+    public float projectileLifetime;
+    public int projectileMaxPoolSize;
+
+    public GameObject projectilePrefab;
+    private GameObject projectilePoolParent;
+    private GameObject[] projectilePool;
+    private Plane mousePlane;
+    // TODO make a rolling index for this, just checking index does not work! Shooting is pretty broken at the moment
+    private int projectilesActive;
+
+    void Awake()
+    {
+        projectilePoolParent = new GameObject("Projectile Pool");
+        projectilePool = new GameObject[projectileMaxPoolSize];
+
+        for (int i = 0; i < projectileMaxPoolSize; ++i)
+        {
+            projectilePool[i] = GameObject.Instantiate(projectilePrefab, projectilePoolParent.transform);
+            projectilePool[i].SetActive(false);
+        }
+
+        mousePlane = new Plane(Vector3.up, 0f);
+    }
 
     void Start()
     {
@@ -14,10 +44,46 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
-        rigidbody.velocity = new Vector3(
-                Input.GetAxis("Horizontal") * speed,
+        // Super basic movement
+        rigidbody.velocity = Vector3.ClampMagnitude(new Vector3(
+                Input.GetAxisRaw("Horizontal") * speed,
                 rigidbody.velocity.y,
-                Input.GetAxis("Vertical") * speed
-        );
+                Input.GetAxisRaw("Vertical") * speed), speed);
+
+        // Shooty logic
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 worldPos = Vector3.zero;
+
+            // Ensure we intercept mouse pos at the same height as the player
+            mousePlane.SetNormalAndPosition(Vector3.up, new Vector3(0f, transform.position.y, 0f));
+
+            // Shoot a ray to the plane and figure out what that world pos is (fast and efficient, no collider checks)
+            Ray mouseRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            float distance;
+            if (mousePlane.Raycast(mouseRay, out distance))
+            {
+                worldPos = mouseRay.GetPoint(distance);
+                // Debug.DrawLine(Camera.main.ScreenToWorldPoint(Input.mousePosition),
+                //         worldPos, Color.red, 5f);
+            }
+
+            if (projectilesActive < projectileMaxPoolSize)
+            {
+                GameObject projectileGO = projectilePool[projectilesActive];
+                projectileGO.SetActive(true);
+                Projectile projectile = projectileGO.GetComponent<Projectile>();
+                projectile.Setup(this, transform.position, (worldPos - transform.position).normalized, projectileSpeed, projectileLifetime, 1f);
+
+                ++projectilesActive;
+            }
+        }
+
+    }
+
+    public void ReportDeadProjectile()
+    {
+        --projectilesActive;
     }
 }
---
+++ Assets/Scripts/Gameplay/Projectile.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    // TODO should projectiles have prefabs/ScriptableObjects with public inspector-assigned values?
+    // Probably yeah - do it later, too sleepy
+    private float movementSpeed;
+    private Vector3 movementDirection;
+
+    private float damageValue;
+
+    private float lifetime;
+    private float lifetimeStart;
+
+    private PlayerController player;
+
+    void Update()
+    {
+        if (gameObject.activeSelf)
+        {
+            transform.position += movementDirection * movementSpeed * Time.deltaTime;
+
+            if (lifetimeStart + lifetime < Time.timeSinceLevelLoad)
+            {
+                player.ReportDeadProjectile();
+
+                gameObject.SetActive(false);
+            }
+        }
+    }
+
+    public void Setup(PlayerController source, Vector3 startPos, Vector3 direction, float speed, float maxLifetime, float damage)
+    {
+        player = source;
+        transform.position = startPos;
+        movementSpeed = speed;
+        movementDirection = direction;
+        lifetime = maxLifetime;
+        lifetimeStart = Time.timeSinceLevelLoad;
+        damageValue = damage;
+    }
+}
